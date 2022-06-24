b41c10e1655790d3cf0d80d1c7e40bea84325b4a
anipvin <anipvin@gmail.com>
2022-06-24 23:15:06 +0800

    Footer
    footer html and css

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,5 +26,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     
 </body>
- 
+ <style>
+    .custom-login{
+        height: 350px;
+        padding-top: 50px;
+    }
+ </style>
 </html>  
